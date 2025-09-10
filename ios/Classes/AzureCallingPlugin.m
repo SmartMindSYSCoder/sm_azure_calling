eb987698949ab88857bf221726e35696af81fa87
Amjad Alhitary <83473041+Amjedgit@users.noreply.github.com>
2025-09-10 21:32:46 +0300

    Update AzureCallingPlugin.m

--- ios/Classes/AzureCallingPlugin.m
+++ ios/Classes/AzureCallingPlugin.m
@@ -1,11 +1,13 @@
-#import <Flutter/Flutter.h>
+(* #import <Flutter/Flutter.h>
 #import "sm_azure_calling-Swift.h"   // auto-generated header, matches your plugin name
 
 @interface AzureCallingPlugin : NSObject<FlutterPlugin>
 @end
 
 @implementation AzureCallingPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-    [SwiftAzureCallingPlugin registerWithRegistrar:registrar];
-}
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)
+(* registrar { *)
+(*     [SwiftAzureCallingPlugin registerWithRegistrar:registrar];
+(* } *)
 @end
+ *) *)
